de71a6077b6b524c4656e805d8af09faeafccdcb
Eric Tempest <eric.tempest@bentley.com>
2025-07-24 12:29:06 -0600

    wip

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
-# zshrc is stored in git. Must link and reload to configure brew
-ln zsh/.zshrc ~/.zshrc
-source ~/.zshrc
+# OMZ
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended 
+cat zsh/.zshrc >> ~/.zshrc
 
 # homebrew
+export HOMEBREW_BUNDLE_FILE="~/dotfiles/brew/Brewfile"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew bundle install brew/BrewFile
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
+brew bundle install
 
